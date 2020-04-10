00ce47fc911a793a53de747b62cbaf5754cf9e74
ikenna agulobi <ikennaww@yahoo.fr>
2020-04-10 21:00:10 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -13,7 +13,7 @@ introduction ("Ikenna ")
 
 def introduction_with_language(name , language)
   
-  puts "Hi, my name is #{name} and i am learning to program in#{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
 
 
